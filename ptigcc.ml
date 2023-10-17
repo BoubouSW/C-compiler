503d30ab24a5ba086b3f12db2a7169b1d31e68f1
Axel Vivenot <avivenot-23@tp-1a252-15.enst.fr>
2023-10-17 15:33:13 +0200

    Adaptation du fichier ptipython à notre projet

---
+++ ptigcc.ml
@@ -0,0 +1,22 @@
+open Printf
+  
+let _main = 
+  (* Vérification de la ligne de commande *)
+  if Array.length Sys.argv <> 2 then
+    begin
+      eprintf "usage: ./ptigcc.exe [file].c\n" ;
+      exit 1
+    end ;
+
+   let ifile = Sys.argv.(1) in
+  
+  (* Vérification de l'extension .c *)
+  if not (Filename.check_suffix ifile ".c") then begin
+    eprintf "Le fichier d'entrée doit avoir l'extension .c\n";
+    exit 1
+  end;
+  Reader.read_file ifile 
+
+
+
+
