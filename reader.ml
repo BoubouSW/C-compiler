9ecad294b23baeaec753fe260c65c7f39d78b1e0
Axel Vivenot <avivenot-23@tp-1a252-15.enst.fr>
2023-10-17 16:39:26 +0200

    reader:ml : suppression des OPEN inutiles

--- reader.ml
+++ reader.ml
@@ -1,21 +1,16 @@
-open Lexing
-
-open Compiler
-open Lexer
-open Parser
-
 let read_file ifile =
   (*On ouvre le fichier*)
   let file = open_in ifile in
 
-  
   let buffer = Lexing.from_channel file in
 
   (*On transforme le buffer en arbre de tokens*)
-  (*let parsed = Parser.parse buffer*)
+  let _ = Parser.file Lexer.token buffer in
 
   (*On ferme le fichier*)
   close_in file;
 
   (*On compile le résultat du parser*)
   (*Compiler.compile parsed*)
+
+  ()
