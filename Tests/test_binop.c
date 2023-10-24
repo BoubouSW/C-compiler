e6fdc23a2db0160afdcb12cc40ed8cbad078c3a3
Axel Vivenot <avivenot-23@tp-1a252-30.enst.fr>
2023-10-24 15:03:49 +0200

    debut fonctions de test

--- Tests/test_binop.c
+++ Tests/test_binop.c
@@ -1,6 +1,12 @@
 int main(){
-    
+    printint(1 * 8 + 6 - 5 / 3 % 2);
+    printint(13);
+    printint(5 / 8 * 2 % 7 - 1 + 9);
+    printint(8);
+    printint(-8 * 2 % 3 + 5 - 1 / 4);
+    printint(6);
+    print( 1 - 9 % 4 / 6 + 2 * 4);
+    printint(9);
 
-    print(1 * 8 + 6 - 5 / 3 % 2);
     return 0;
 }
--- Tests/test_variables.c
+++ Tests/test_variables.c
@@ -1,10 +1,3 @@
-int test(int x) {
-    printint(5);
-    return 0;
-}
-
 int main() {
-    test(5);
-    1+1;
     return 0;
 }
