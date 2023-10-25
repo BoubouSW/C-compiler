0dbb0a2902663f0b6a4f492d629e0283de66dac0
Yann GIREY <yann.girey@telecom-paris.fr>
2023-10-25 09:41:56 +0200

    correction et augmentation de la lisibilite du programme et ajout des retour a la ligne

--- c_to_mips.ml
+++ c_to_mips.ml
@@ -1,7 +1,7 @@
 open Ast_c
 open Ast_mips
 
-let variables  = Hashtbl.create 100
+
 
 let associe_binop op = match op with
   |Mul -> Mulm
@@ -9,56 +9,104 @@ let associe_binop op = match op with
   |Add -> Addm
   | _ -> print_string "Pas codee associe_binop";failwith "Pas un binop"
 
-let converti program = (*On stocke le resultat dans a*)
+
+
+let converti program = (*On stocke le resultat des instructions dans a(0)*)
   
-  let rec eval_binop op e1 e2 off_set = match op,e1,e2 with
+  let rec eval_binop op e1 e2 off_set var_locales= match op,e1,e2 with
+
+    (*On separe les cas ou on additionne une constante*)
     |Add,Const(Inti(i)),e | Add,e,Const(Inti(i)) 
-     -> (eval_expr e off_set)@[Sbinopi(Addi,A(0),A(0),Intm(i))]
+     -> (eval_expr e off_set var_locales)@[Sbinopi(Addi,A(0),A(0),Intm(i))]
     
-    |Sub,e,Const(Inti (i)) -> (eval_expr e off_set)@[Sbinopi(Addi,A(0),A(0),Intm(-i))]
+    |Sub,e,Const(Inti (i)) -> (eval_expr e off_set var_locales)@[Sbinopi(Addi,A(0),A(0),Intm(-i))]
 
-    |Mul,_,_|Sub,_,_|Add,_,_ -> (eval_expr e1 off_set)@[Sbinopi(Sw,A(0),Sp,Intm(-4*off_set))]
-      @(eval_expr e2 (off_set+1))@[Sbinopi(Lw,T(0),Sp,Intm(-4*off_set));Sbinop(associe_binop op,A(0),A(0),T(0))]
+    |Mul,_,_|Sub,_,_|Add,_,_ -> (eval_expr e1 off_set var_locales)
+      @[Sbinopi(Sw,A(0),Sp,Intm(-4*off_set))]
+      @(eval_expr e2 (off_set+1) var_locales)
+      @[Sbinopi(Lw,T(0),Sp,Intm(-4*off_set));Sbinop(associe_binop op,A(0),A(0),T(0))]
 
-    |Div,_,_ -> (eval_expr e1 off_set)@[Sbinopi(Sw,A(0),Sp,Intm(-4*off_set))]@
-    (eval_expr e2 (off_set+1))@[Sbinopi(Lw,T(0),Sp,Intm(-4*off_set));Smonop(Divm,A(0),T(0));Smonop(Smf,A(0),Hi)]
+    |Div,_,_ -> (eval_expr e1 off_set var_locales)
+      @[Sbinopi(Sw,A(0),Sp,Intm(-4*off_set))]
+      @(eval_expr e2 (off_set+1) var_locales)
+      @[Sbinopi(Lw,T(0),Sp,Intm(-4*off_set));Smonop(Divm,T(0),A(0));Smonop(Smf,A(0),Hi)]
 
-    |Mod,_,_ -> (eval_expr e1 off_set)@[Sbinopi(Sw,A(0),Sp,Intm(-4*off_set))]@
-    (eval_expr e2 (off_set+1))@[Sbinopi(Lw,T(0),Sp,Intm(-4*off_set));Smonop(Divm,A(0),T(0));Smonop(Smf,A(0),Lo)]
+    |Mod,_,_ -> (eval_expr e1 off_set var_locales)
+      @[Sbinopi(Sw,A(0),Sp,Intm(-4*off_set))]
+      @(eval_expr e2 (off_set+1) var_locales)
+      @[Sbinopi(Lw,T(0),Sp,Intm(-4*off_set));Smonop(Divm,T(0),A(0));Smonop(Smf,A(0),Lo)]
 
     |_ -> print_string "Pas codee eval_binop"; failwith "Pascodee "
 
-    and eval_expr e off_set = match e with
-    |Minus(expr) -> eval_binop Sub (Const(Inti 0)) expr off_set
+
+
+
+    and eval_expr e off_set var_locales = match e with
+    |Minus(expr) -> eval_binop Sub (Const(Inti 0)) expr off_set var_locales
+
     |Const(Inti(i)) -> [Smonopi(Li,A(0),Intm(i))]
-    |Op(b,e1,e2) -> eval_binop b e1 e2 off_set
-    |Ecall(f,l) -> 
+
+    |Op(b,e1,e2) -> eval_binop b e1 e2 off_set var_locales
+
+    |Ecall(f,l) ->
+      
       let assigne_les_variables = List.concat (List.mapi 
         (fun i arg ->
-          (eval_expr arg (off_set+i+1))@[Sbinopi(Sw,A(0),Sp,Intm(-4*(off_set+i+1)))]) l) in
-      [Sbinopi(Addi,Sp,Sp,Intm(-4*off_set))]@assigne_les_variables@[Sjump(Jal(f));Sbinopi(Addi,Sp,Sp,Intm(4*off_set))]
+          (eval_expr arg (off_set+i+1) var_locales)@[Sbinopi(Sw,A(0),Sp,Intm(-4*(off_set+i+1)))]) l) in
+
+          (*On deplace Sp a la fin de la stack, et on met a l'indice 0 ra, et aux indices suivant les valeurs des arguments de la fonction*)
+      assigne_les_variables@[Sbinopi(Addi,Sp,Sp,Intm(-4*(off_set)))]@[Sjump(Jal(f));Sbinopi(Addi,Sp,Sp,Intm(4*(off_set)))]
+
     |Const(Null) -> []
+
     |Var(s) -> 
-      (match Hashtbl.find_opt variables s with
+      (match Hashtbl.find_opt var_locales s with
         |Some(Intm(n)) -> [Sbinopi(Lw,A(0),Sp,Intm(-4*n))] 
         |_ -> print_string ("variable "^s^" non definie\n");
     failwith "undefined")
+
     |_ -> print_string "Pas codee eval_expr";failwith "Pascodee "
 
-    and eval_stmt ?(main=false) stmt off_set = match stmt with
-    |Sblock(l) -> List.fold_left (fun instr s-> instr@(eval_stmt ~main:false s off_set)) [] l
-    |Sval(e) -> eval_expr e off_set
-    |Sprintint(e) -> (eval_expr e off_set) @ [Smonopi(Li,V0,Intm(1));Ssyscall]
-    |Sreturn(e) when main = true -> (eval_expr e off_set)@[Smonopi(Li,V0,Intm(10));Ssyscall]
-    |Sreturn(e) -> (eval_expr e off_set)@[Sbinopi(Lw,Ra,Sp,Intm(0));Sjump(Jr(Ra))]
+
+
+
+    and eval_stmt ?(main=false) var_locales stmt off_set = match stmt with
+
+    |Sblock(l) -> List.fold_left (fun instr s-> instr@(eval_stmt ~main:main var_locales s off_set)) [] l
+
+    |Sval(e) -> eval_expr e off_set var_locales
+
+    |Sprintint(e) -> 
+      (eval_expr e off_set var_locales) (*Evalue l'expression*)
+      @ [Smonopi(Li,V0,Intm(1));Ssyscall; (*Print l'expression*)
+      Smonopi(Li,A(0),Intm(10));Smonopi(Li,V0,Intm(11));Ssyscall] (*retour a la ligne*)
+
+    |Sreturn(e) when main = true -> (eval_expr e off_set var_locales)@[Smonopi(Li,V0,Intm(10));Ssyscall]
+
+    |Sreturn(e) -> (eval_expr e off_set var_locales)@[Sbinopi(Lw,Ra,Sp,Intm(0));Sjump(Jr(Ra))]
+
     |_ -> print_string "Pas codee eval_stmt"; failwith "Pascodee "
 
   in 
     List.fold_left 
     (fun instr fonction ->
-      List.iteri (fun i (Args(_,arg)) -> Hashtbl.add variables arg (Intm(i+1))) fonction.args;
-      let evalue_la_fonction =eval_stmt ~main:(fonction.name="main") fonction.body (1+(List.length fonction.args)) in
-      List.iter (fun (Args(_,arg)) -> Hashtbl.remove variables arg) fonction.args;
-      instr@[Slabel(fonction.name);Sbinopi(Sw,Ra,Sp,Intm(0))]
-      @evalue_la_fonction)
-    [] program.defs
+      (* On ajoute les arguments de la fonction dans le contexte*)
+      let variables_locales  = Hashtbl.create 100 in
+      List.iteri (fun i (Args(_,arg)) -> Hashtbl.add variables_locales arg (Intm(i+1))) fonction.args;
+      let corps_de_la_fonction = eval_stmt ~main:(fonction.name="main") variables_locales fonction.body (1+(List.length fonction.args)) in 
+      (* L'argument main est à true lorsque la fonction etudiee est main, et donc ajoute a la fin de la fonction un syscall pour exit*)
+      (* Les premiers elements de la pile sont occupes par les arguments et ra*)
+
+      (* On retire les variables locales du contexte*)
+
+      if fonction.name <> "main"
+      then 
+        instr
+        @[Slabel(fonction.name);Sbinopi(Sw,Ra,Sp,Intm(0))]
+        @corps_de_la_fonction
+      else 
+        instr
+        @[Slabel("main")]
+        @corps_de_la_fonction)
+
+    [] program.defs (* Le list.fold_left est equivalent a un List.concat List.map*)
