daf7e4ab4541ea4b900e653115fbb2170206b895
JamesCamilleri13 <118368455+JamesCamilleri13@users.noreply.github.com>
2023-10-30 16:45:50 +0100

    ajout tests

--- Tests/test_variables.c
+++ Tests/test_variables.c
@@ -1,8 +1,18 @@
+int f(int x){
+    return x+1;
+}
+
+int g(int x){
+    return 2*x;
+}
+
 int main() {
     int x;
     x = 10;
     int y;
     y = 9;
-    printint(x+y);
+    printint(x+y); // 19
+    printint(g(f(x+3))); // 28
+
     return 0;
 }
